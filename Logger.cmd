e60d4403245b310f53bf0806d857b0e55dc9da9c
AT-3109 <116757809+AT-3109@users.noreply.github.com>
2024-05-11 11:30:10 +0100

    Update Logger.cmd
    Add arp -a and date /t

--- Logger.cmd
+++ Logger.cmd
@@ -11,7 +11,12 @@ cls
 ::log process
 mkdir output
 cd output
+date /t >> hardware.txt
 ipconfig /all >> hardware.txt
+arp -a >> hardware.txt
+getmac /fo csv >> hardware.txt
 systeminfo >> hardware.txt
+date /t >> connections.txt
 netstat /a >> connections.txt
+date /t >> processes.txt
 tasklist >> processes.txt
